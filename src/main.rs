8e09d1a7f94f654b85661c9e28b3c1810a114891
rsookram <rashadsookram@gmail.com>
2020-04-23 22:04:06 -0400

    Allow multiple files to be processed at once

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,11 @@ use std::collections::BTreeMap;
 struct Attribute<'a>((Option<&'a str>, &'a str));
 
 fn main() {
-    // TODO: Allow 0 or more paths
-    let path = std::env::args()
-        .nth(1)
-        .expect("missing required path to XML file");
+    // TODO: Use structopt
+    let mut args = std::env::args();
+    args.next();
+
+    let paths = args;
 
     // TODO: Parse from config, use something like TOML
     let raw_requirements: Vec<(&str, Attribute)> = vec![("LinearLayout", "android:orientation")]
@@ -22,57 +23,59 @@ fn main() {
         })
         .collect();
 
-    let content = std::fs::read_to_string(&path).unwrap();
-
-    let doc = Document::parse(&content).unwrap();
-
-    let namespaces: BTreeMap<_, _> = doc
-        .root_element()
-        .namespaces()
-        .iter()
-        .filter_map(|ns| ns.name().map(|name| (name, ns.uri())))
-        .collect();
-
-    let requirements: BTreeMap<&str, Attribute> = raw_requirements
-        .iter()
-        .map(|(tag, attr)| {
-            let Attribute((ns, name)) = attr;
-            let ns = ns.and_then(|ns| {
-                let resolved = namespaces.get(ns).copied();
-
-                resolved.or(Some(ns))
-            });
-
-            (*tag, Attribute((ns, name)))
-        })
-        .collect();
-
     let mut meets_requirements = true;
-    doc.descendants()
-        .filter_map(|n| {
-            if let Some(attr) = requirements.get(n.tag_name().name()) {
-                let ex_name = to_expanded_name(attr);
+    for path in paths {
+        let content = std::fs::read_to_string(&path).unwrap();
 
-                if n.has_attribute(ex_name) {
-                    None
+        let doc = Document::parse(&content).unwrap();
+
+        let namespaces: BTreeMap<_, _> = doc
+            .root_element()
+            .namespaces()
+            .iter()
+            .filter_map(|ns| ns.name().map(|name| (name, ns.uri())))
+            .collect();
+
+        let requirements: BTreeMap<&str, Attribute> = raw_requirements
+            .iter()
+            .map(|(tag, attr)| {
+                let Attribute((ns, name)) = attr;
+                let ns = ns.and_then(|ns| {
+                    let resolved = namespaces.get(ns).copied();
+
+                    resolved.or(Some(ns))
+                });
+
+                (*tag, Attribute((ns, name)))
+            })
+            .collect();
+
+        doc.descendants()
+            .filter_map(|n| {
+                if let Some(attr) = requirements.get(n.tag_name().name()) {
+                    let ex_name = to_expanded_name(attr);
+
+                    if n.has_attribute(ex_name) {
+                        None
+                    } else {
+                        Some((n, ex_name))
+                    }
                 } else {
-                    Some((n, ex_name))
+                    None
                 }
-            } else {
-                None
-            }
-        })
-        .for_each(|(n, attr)| {
-            meets_requirements = false;
+            })
+            .for_each(|(n, attr)| {
+                meets_requirements = false;
 
-            println!(
-                "{}:{} {} missing {} attribute",
-                path,
-                doc.text_pos_at(n.range().start),
-                n.tag_name().name(),
-                attr.name()
-            )
-        });
+                println!(
+                    "{}:{} {} missing {} attribute",
+                    path,
+                    doc.text_pos_at(n.range().start),
+                    n.tag_name().name(),
+                    attr.name()
+                )
+            });
+    }
 
     if !meets_requirements {
         std::process::exit(1);
