1bc26d944e350e94782d318787b5b2e967daca56
rsookram <rashadsookram@gmail.com>
2020-04-27 22:00:01 -0400

    Improve formatting of error messages

--- src/error.rs
+++ src/error.rs
@@ -16,30 +16,30 @@ impl std::error::Error for Error {}
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
-            Error::ReadConfig(path, cause) => write!(
+            Error::ReadConfig(path, cause) => write_error(
                 f,
-                "Error: Failed to read {}: {}",
-                path.to_string_lossy(),
-                cause
+                &format!("Failed to read {}", path.to_string_lossy()),
+                cause,
             ),
-            Error::ParseConfig(path, cause) => write!(
+            Error::ParseConfig(path, cause) => write_error(
                 f,
-                "Error: Failed to parse config {}: {}",
-                path.to_string_lossy(),
-                cause
+                &format!("Failed to parse config {}", path.to_string_lossy()),
+                cause,
             ),
-            Error::ReadXML(path, cause) => write!(
+            Error::ReadXML(path, cause) => write_error(
                 f,
-                "Error: Failed to read XML file {}: {}",
-                path.to_string_lossy(),
-                cause
+                &format!("Failed to read XML file {}", path.to_string_lossy()),
+                cause,
             ),
-            Error::ParseXML(path, cause) => write!(
+            Error::ParseXML(path, cause) => write_error(
                 f,
-                "Error: Failed to parse XML file {}: {}",
-                path.to_string_lossy(),
-                cause
+                &format!("Failed to parse XML file {}", path.to_string_lossy()),
+                cause,
             ),
         }
     }
+}
+
+fn write_error(f: &mut fmt::Formatter, msg: &str, cause: &str) -> fmt::Result {
+    write!(f, "Error: {}\n\nCause: {}", msg, cause)
 }
