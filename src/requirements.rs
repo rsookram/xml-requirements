9e5e1a00353ec24a0a8f3653c52e9e101338a515
rsookram <rashadsookram@gmail.com>
2020-04-30 21:29:20 -0400

    Add simple tests for requirements

--- src/requirements.rs
+++ src/requirements.rs
@@ -6,6 +6,7 @@ use std::collections::BTreeMap;
 
 pub type Requirements<'a> = BTreeMap<&'a str, Vec<ResolvedName<'a>>>;
 
+#[derive(Debug, PartialEq)]
 pub struct ResolvedName<'a> {
     pub raw: String,
     pub expanded: ExpandedName<'a>,
@@ -41,5 +42,99 @@ fn resolve_attr<'a>(attr: &'a Attribute, doc: &'a Document) -> ResolvedName<'a> 
     ResolvedName {
         raw: attr.raw.to_string(),
         expanded,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::config::Rule;
+
+    #[test]
+    fn empty_config() -> Result<(), roxmltree::Error> {
+        let config = BTreeMap::new();
+
+        let doc = Document::parse(
+            r#"
+            <LinearLayout
+                xmlns:android="http://schemas.android.com/apk/res/android"
+                android:layout_width="match_parent"
+                android:layout_height="match_parent" />
+            "#,
+        )?;
+
+        let reqs = resolve(&config, &doc);
+
+        assert!(reqs.is_empty());
+
+        Ok(())
+    }
+
+    #[test]
+    fn resolve_name_with_ns() -> Result<(), Box<dyn std::error::Error>> {
+        let config = vec![(
+            "EditText".to_string(),
+            Rule {
+                required: vec!["android:hint".parse()?],
+            },
+        )]
+        .into_iter()
+        .collect();
+
+        let doc = Document::parse(
+            r#"
+            <LinearLayout
+                xmlns:android="http://schemas.android.com/apk/res/android"
+                android:layout_width="match_parent"
+                android:layout_height="match_parent" />
+            "#,
+        )?;
+
+        let reqs = resolve(&config, &doc);
+
+        assert_eq!(1, reqs.keys().len());
+        assert_eq!(
+            vec![ResolvedName {
+                raw: "android:hint".to_string(),
+                expanded: ("http://schemas.android.com/apk/res/android", "hint").into()
+            }],
+            reqs["EditText"]
+        );
+
+        Ok(())
+    }
+
+    #[test]
+    fn resolve_name_without_ns() -> Result<(), Box<dyn std::error::Error>> {
+        let config = vec![(
+            "Button".to_string(),
+            Rule {
+                required: vec!["style".parse()?],
+            },
+        )]
+        .into_iter()
+        .collect();
+
+        let doc = Document::parse(
+            r#"
+            <LinearLayout
+                xmlns:android="http://schemas.android.com/apk/res/android"
+                android:layout_width="match_parent"
+                android:layout_height="match_parent" />
+            "#,
+        )?;
+
+        let reqs = resolve(&config, &doc);
+
+        assert_eq!(1, reqs.keys().len());
+        assert_eq!(
+            vec![ResolvedName {
+                raw: "style".to_string(),
+                expanded: ("style").into()
+            }],
+            reqs["Button"]
+        );
+
+        Ok(())
     }
 }
